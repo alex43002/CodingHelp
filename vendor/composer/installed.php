4e97e3b380789514c7cee68c7cc4d0ab94352088
alex43002 <alexdejesus336@Yahoo.com>
2023-03-14 05:58:20 -0400

    Update Lock File and dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'coding-help/alexander-dejesus',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c9c6943a22a0da9f06f4b45e9274f1fb0788bc1e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'coding-help/alexander-dejesus' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c9c6943a22a0da9f06f4b45e9274f1fb0788bc1e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
